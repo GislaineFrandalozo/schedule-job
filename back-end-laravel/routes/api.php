84250e59ab39ed2d1e063e25c136f1c44b571811
Gislaine Frandalozo <frandalozo.gislaine@gmail.com>
2022-11-02 23:05:28 -0300

    testing API

--- back-end-laravel/routes/api.php
+++ back-end-laravel/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/rules-enabled', function (Request $request) {
+   $user = $request->user();
+    return [
+    'user' => $user
+];
+})->middleware(['auth:sanctum', 'ability:app-admin,app-user']);
+Route::get('/teste', function (Request $request) {
+    
+ 
+    return [];
+});
+Route::post('/tokens/create', function (Request $request) {
+    
+    $user = User::where('name', $request->header('user'))->first();
+    $token = $user->createToken($request->header('user'), ['app-user']);
+    return ['token' => $token->plainTextToken];
 });
